49a37c60d4379e10f67e0852f22fd658c763f22b
Mohamed Ndayishimiye <ndayimodi@gmail.com>
2023-12-10 23:36:02 +0100

    Add docs folder

--- test.js
+++ test.js
@@ -1,7 +1,17 @@
-// const name= 'Ali';
-// console.log(name);
-const greet = (name)=>{
-    console.log(`hello, ${name}`);
+// let counter = 0;
+// const numbers= setInterval(() => {
+//   counter++;
+//   console.log(counter);
+// }, 1000); // 1 second
+
+// setTimeout(()=>{
+//   clearInterval(numbers);
+// }, 2000);
+
+const fs = require('fs');
+fs.readFile('./docs/blogTest.txt', (error,data)=>{
+if(error) {
+  console.log('Theres been an error');
 }
-greet('Mohamed');
-console.log(global)
+console.log(data, toString());
+});
